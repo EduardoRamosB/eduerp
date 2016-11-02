ffb40981c8d5d5a44fd6ac951d38ce03bcd9b8dd
EduardoRamosB <eduardo.ramos.b1@gmail.com>
2016-11-02 11:37:53 -0500

    first commit

---
+++ src/main/java/com/simularte/EduerpApplication.java
@@ -0,0 +1,12 @@
+package com.simularte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduerpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduerpApplication.class, args);
+	}
+}
