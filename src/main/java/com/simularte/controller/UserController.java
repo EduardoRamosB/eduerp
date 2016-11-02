cace14fb73339c119847cac7c2ff7d33f4fcfd58
EduardoRamosB <eduardo.ramos.b1@gmail.com>
2016-11-02 11:59:44 -0500

    setup 0.1

---
+++ src/main/java/com/simularte/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.simularte.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(HttpServletRequest req){
+		return "index";
+	}
+}
